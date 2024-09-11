c1541de98ab9bc6c9b2c3ba7535bc3edb1cb42bb
Senshi <sayonbra@gmail.com>
2024-09-11 13:42:43 +0200

    Feat: styled Mode page

--- app/mode.js
+++ app/mode.js
@@ -3,15 +3,17 @@ import { Link } from "expo-router";
 
 export default function Index() {
 
-    const image = {uri: 'https://github.com/IvanZaja/lol-ramdomizer/blob/master/assets/Images/grieta.webp'};
+    const imageDefault = {uri: 'https://github.com/IvanZaja/lol-ramdomizer/blob/master/assets/Images/grieta.webp?raw=true'};
+    const imageAram = {uri: 'https://github.com/IvanZaja/lol-ramdomizer/blob/adbb5715ff040e16ffe364f856f10cb2c09efb39/assets/Images/aram.jpg?raw=true'};
+
 
     return (
         <View className="flex-1 bg-black items-center justify-center h-full w-full">
             <Link asChild href="/champ" className="text-white text-4xl h-1/2 w-full">
-                <Pressable className="h-20 bg-[#1e2328] border-2 border-[#c4a464] flex items-center justify-center shadow-md shadow-black" ><ImageBackground source={image} resizeMode="cover" style={styles.image}><Text className="text-white text-4xl p-4">Grieta</Text></ImageBackground></Pressable>
+                <Pressable className="h-20 flex items-center justify-center shadow-md shadow-black" ><ImageBackground source={imageDefault} resizeMode="cover" style={styles.image} className="flex items-center justify-center"><Text className="text-white text-4xl p-4 opacity-100">Grieta</Text></ImageBackground></Pressable>
             </Link>
             <Link asChild href="/champAram" className="text-white text-4xl h-1/2 w-full">
-                <Pressable className=" h-20 bg-[#1e2328] border-2 border-[#c4a464] flex items-center justify-center shadow-md shadow-black" ><Text className="text-white text-4xl p-4">Aram</Text></Pressable>
+                <Pressable className=" h-20 flex items-center justify-center shadow-md shadow-black" ><ImageBackground source={imageAram} resizeMode="cover" style={styles.image} className="flex items-center justify-center"><Text className="text-white text-4xl p-4">Aram</Text></ImageBackground></Pressable>
             </Link>
         </View>
     )
