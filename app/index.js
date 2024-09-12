cc0f45ec01547904713e259769f77e96bf15514b
Senshi <sayonbra@gmail.com>
2024-09-12 15:48:37 +0200

    Added logo

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { Button, ImageBackground, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, Image, ImageBackground, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Link } from "expo-router";
 import ReloadIcon from '../components/Icons/ReloadIcon';
 
@@ -9,7 +9,7 @@ export default function Index() {
     return (
         <View className="flex-1 bg-black items-center justify-start">
             <ImageBackground source={image} resizeMode="cover" style={styles.image} className="flex items-center justify-center">
-                <ReloadIcon className="w-3/6 h-3/6 absolute top-10"/>
+                <Image source={{uri: 'https://github.com/IvanZaja/lol-ramdomizer/blob/master/assets/app/LolRandomizerLogo.png?raw=true'}} className="w-3/6 h-3/6 absolute top-10" />
                 <Link asChild href="/mode" className="text-white text-4xl absolute top-[55%]">
                     <Pressable className="w-2/3 h-20 bg-[#1e2328] border-2 border-[#c4a464] flex items-center justify-center shadow-md shadow-black" ><Text className="text-white text-4xl p-4">Comenzar</Text></Pressable>
                 </Link>
